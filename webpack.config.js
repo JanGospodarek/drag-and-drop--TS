9a47966c136dc6158d5ca5c1dad1982399770205
JanGospodarek <jan.gospo@onet.pl>
2022-10-10 13:47:50 +0200

    adding webpack

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require("path");
+module.exports = {
+  entry: "./src/app.ts",
+  output: {
+    filename: "boundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {},
+};
